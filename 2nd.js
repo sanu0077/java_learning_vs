dadded7165938cc444dc79cf04f90c05c693a0bb
sanu0077 <sunayana4112@gmail.com>
2024-08-01 21:31:05 +0530

    2nd

--- 2nd.js
+++ 2nd.js
@@ -7,3 +7,6 @@ console.log(typeof "hi");
 console.log(typeof false);
 console.log(typeof 343);
 console.log(typeof hi);
+
+
+// in boolean - > 1 is true, 0 is false , empty is false, string is true.
