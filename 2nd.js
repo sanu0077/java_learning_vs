74f6cff3bd4c37637a456cf89ff64556ec4d0aad
sanu0077 <sunayana4112@gmail.com>
2024-08-01 21:25:43 +0530

    2nd

--- 2nd.js
+++ 2nd.js
@@ -1,1 +1,9 @@
-console.log("hii");
+console.log("hii guys");
+
+
+console.log(typeof true);
+console.log(typeof 33);
+console.log(typeof "hi");
+console.log(typeof false);
+console.log(typeof 343);
+console.log(typeof hi);
