d44b9a43b67024ea85a5a09091c15040d8fe47ee
sanu0077 <sunayana4112@gmail.com>
2024-08-02 10:57:26 +0530

    operations

---
+++ operations.js
@@ -0,0 +1,23 @@
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+console.log(2+2);
+console.log(2-2);
+console.log(2/2);
+console.log(2*2);
+console.log(2**3); // ** consider as to the power
+console.log(2%3);
+
+let str1 = "Hi"
+let str2 = " guys"
+let str3 = str1 + str2
+console.log(str3);
+
+
+console.log("2"+2);
+console.log(2+"2");
+console.log("2"+2+2);
+console.log(2+2+"2");
+
+
